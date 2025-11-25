beaa6e138c8f6d7e0d301a68ed52988c2ceb0978
Vitor Vargas <vitorrodriguesvargas@gmail.com>
2025-11-25 12:34:51 -0300

    config-supabase

---
+++ Migrations/20251125152202_supabase.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace icone_backend.Migrations
+{
+    /// <inheritdoc />
+    public partial class supabase : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
